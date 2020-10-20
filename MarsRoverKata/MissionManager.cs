4e1520cff64f1358cc7e0879c77a96e68860fa4b
Favorut123 <volodarchuhay@gmail.com>
2020-10-20 10:58:54 +0300

    1

---
+++ MarsRoverKata/MissionManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using MarsRoverKata.Commands;
+
+namespace MarsRoverKata
+{
+    public class MissionManager
+    {
+        public Rover Rover { get; private set; }
+        public Mars Mars { get; private set; }
+        public Size Bounds { get { return Mars.Bounds; } }
+        public IReadOnlyList<Obstacle> Obstacles { get { return Mars.Obstacles; } }
+
+        private readonly Commander _commander;
+        
+        public MissionManager(Mars mars, Rover rover)
+        {
+            Mars = mars;
+            Rover = rover;
+            _commander = new Commander();
+        }
+
+        public string AcceptCommands(String commandString)
+        {
+            _commander.Commands = new List<ICommand>();
+
+            var commands = commandString.ToLower().ToCharArray();
+            bool success = true;
+            foreach (Char command in commands)
+            {
+                success = AcceptCommand(success, command);
+            }
+
+            return success ? String.Empty : "Some invalid commands were found!!!";
+        }
+
+        private bool AcceptCommand(bool success, Char command)
+        {
+            if (command.Equals('f'))
+                _commander.Accept(new ForwardCommand(Rover));
+            else if (command.Equals('b'))
+                _commander.Accept(new BackwardCommand(Rover));
+            else if (command.Equals('r'))
+                _commander.Accept(new TurnRightCommand(Rover));
+            else if (command.Equals('l'))
+                _commander.Accept(new TurnLeftCommand(Rover));
+            else
+                success = false;
+            return success;
+        }
+
+        public string ExecuteMission()
+        {
+            bool success = _commander.ExecuteCommands();
+
+            return success ? String.Empty : "An error occured while executing commands";
+        }
+
+        public void AddObstacle(int x, int y)
+        {
+            Point location = new Point(x, y);
+            var obstacle = new Obstacle(location);
+            Mars.AddObstacle(obstacle);
+        }
+    }
+}
---
+++ MarsRoverKata/Rover.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MarsRoverKata
+{
+    public class Rover
+    {
+        public Mars Mars { get; set; }
+        public Point Location { get; set; }
+        public Direction Facing { get; set; }
+
+        private static readonly Dictionary<Direction, Point> PositionalAdjustments = new Dictionary<Direction, Point>()
+        {
+            { Direction.North, new Point(0, 1) },
+            { Direction.South, new Point(0, -1) },
+            { Direction.East, new Point(1, 0) },
+            { Direction.West, new Point(-1, 0) }
+        };
+        
+        public Rover(Mars mars)
+        {
+            Mars = mars;
+            LandOnMars(Mars.MarsCenter);
+        }
+
+        public void LandOnMars(Point landingPoint)
+        {
+            if (!Mars.IsValidPosition(landingPoint))
+                throw new CrashException("Doh! We tried to land on something other than the planet and the rover was destroyed!!!");
+            Location = landingPoint;
+            Facing = Direction.North;
+        }
+
+        public bool MoveBackward()
+        {
+            return Move(-1);
+        }
+
+        public bool MoveForward()
+        {
+            return Move(1);
+        }
+
+        private Point CreateDesiredPosition(int adjustmentFactor)
+        {
+            var adjustment = PositionalAdjustments[Facing] * adjustmentFactor;
+            return Location + adjustment;
+        }
+
+        private bool Move(int adjustmentFactor)
+        {
+            var desiredPosition = CreateDesiredPosition(adjustmentFactor);
+            var newLocation = Mars.CalculateFinalPosition(Location, desiredPosition);
+
+            var success = Location != newLocation;
+
+            Location = newLocation;
+
+            return success;
+        }
+        
+        public bool TurnRight()
+        {
+            if (Facing == Direction.North)
+                Facing = Direction.East;
+            else if (Facing == Direction.South)
+                Facing = Direction.West;
+            else if (Facing == Direction.East)
+                Facing = Direction.South;
+            else if (Facing == Direction.West)
+                Facing = Direction.North;
+
+            return true;
+        }
+
+        public bool TurnLeft()
+        {
+            if (Facing == Direction.North)
+                Facing = Direction.West;
+            else if (Facing == Direction.South)
+                Facing = Direction.East;
+            else if (Facing == Direction.East)
+                Facing = Direction.North;
+            else if (Facing == Direction.West)
+                Facing = Direction.South;
+            
+            return true;
+        }
+    }
+}
